7bd4a1cf56f03a489ca41fca66ae1fe9eb37ef9e
Vincent Deng <vincentdeng.it@gmail.com>
2023-08-01 15:49:42 +1000

    fix sql injection

--- src/main/java/com/veracode/verademo/SQLVulnerabilitiy.java
+++ src/main/java/com/veracode/verademo/SQLVulnerabilitiy.java
@@ -11,8 +11,10 @@ public class SQLVulnerabilitiy {
 
     Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/verademo", "root", "root");
     Scanner scanner = new Scanner(System.in);
-    Statement stmt = conn.createStatement();
-    stmt.executeUpdate("INSERT INTO table (name) VALUES ('" + scanner.nextLine() + "');");
+    PreparedStatement stmt = conn.prepareStatement("INSERT INTO table (name) VALUES (?);");
+    stmt.setString(1, scanner.nextLine());
+
+    stmt.executeUpdate();
     stmt.close();
     
     conn.close();
